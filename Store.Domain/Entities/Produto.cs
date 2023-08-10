bc9b6c1cc8f07180310cd8fd66d91831f74c728c
Erik Coelho <erikcoelhomoraes84@gmail.com>
2023-08-10 13:25:19 -0300

    feat: adicionado entidade e exceção personalizada

---
+++ Store.Domain/Entities/Produto.cs
@@ -0,0 +1,26 @@
+﻿using Store.Domain.Exceptions;
+
+namespace Store.Domain.Entities
+{
+    public class Produto
+    {
+        public Produto(string nome, decimal preco, int quantidadeEstoque)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+            Preco = preco;
+            QuantidadeEstoque = quantidadeEstoque;
+            DataCriacao = DateTime.Now;
+            ValorTotal = preco * quantidadeEstoque;
+
+            InvalidProductException.ThrowIfInvalid(nome, preco, quantidadeEstoque);
+        }
+
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public decimal Preco { get; private set; }
+        public int QuantidadeEstoque { get; private set; }
+        public decimal ValorTotal { get; private set; }
+        public DateTime DataCriacao { get; private set; }
+    }
+}
---
+++ Store.Domain/Exceptions/InvalidProductException.cs
@@ -0,0 +1,20 @@
+﻿namespace Store.Domain.Exceptions
+{
+    public class InvalidProductException: Exception
+    {
+        private const string DefaultErrorMessage = "Produto inválido";
+        public InvalidProductException(string message = DefaultErrorMessage)
+            :base(message)
+        {
+        }
+
+        public static void ThrowIfInvalid(string nome, decimal preco, int quantidadeEstoque)
+        {
+            if(nome == null || nome == "") throw new InvalidProductException("O nome do produto não pode ser nulo");
+
+            if (preco < 0) throw new InvalidProductException("O preço do produto deve ser maior que 0");
+
+            if(quantidadeEstoque < 0) throw new InvalidProductException("A quantidade de produto em estoque não pode ser menor que 0");
+        }
+    }
+}
