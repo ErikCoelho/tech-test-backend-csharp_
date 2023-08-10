f478dab8bed43a478bef6df85853d55fdd401544
Erik Coelho <erikcoelhomoraes84@gmail.com>
2023-08-10 13:34:42 -0300

    feat: implementado testes da entidade

---
+++ Store.Tests/Entities/ProductTests.cs
@@ -0,0 +1,22 @@
+﻿using Store.Domain.Entities;
+using Store.Domain.Exceptions;
+
+namespace Store.Tests.Entities
+{
+    [TestClass]
+    public class ProductTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProductException))]
+        public void Should_return_exception_when_product_is_invalid()
+        {
+            new Produto(null, -2, 3);
+        }
+
+        [TestMethod]
+        public void Should_not_return_exception_when_product_is_valid()
+        {
+            new Produto("t-shirt", 12, 2);
+        }
+    }
+}
